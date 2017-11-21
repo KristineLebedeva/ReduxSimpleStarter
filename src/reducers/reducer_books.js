1015e6f5f7705baa454b34beac43c243746750a4
Other Kristine <kristina.lebedeva@inbox.lv>
2017-11-21 14:46:32 +0200

    Section 4 completed

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: 'book 1', pages: 10},
+        {title: 'book 2', pages: 20},
+        {title: 'book 3', pages: 30},
+        {title: 'book 4', pages: 40}
+    ]
+}
